a98e55bdfe5d1f1114521cd322c2157b321975d6
kolkol <kolodiy1103@gmail.com>
2019-09-18 09:27:18 +0200

    Add more styling to cell, add shadow effect

--- Simple-CollectionView/ViewController.swift
+++ Simple-CollectionView/ViewController.swift
@@ -35,21 +35,32 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let book = store.audiobooks[indexPath.row]
         
         cell.displayContent(image: store.images[indexPath.row], title: book.name)
-        cell.contentView.backgroundColor = UIColor(red: 240/256, green: 240/256, blue: 240/256, alpha: 0.4)
+        cell.contentView.backgroundColor = UIColor.clear
         cell.layer.borderColor = UIColor(red: 240/256, green: 240/256, blue: 240/256, alpha: 1).cgColor
         cell.layer.borderWidth = 1.5
+        
+        cell.layer.cornerRadius = 15.0
+        cell.layer.masksToBounds = true
+        
+        
+        cell.layer.shadowColor = UIColor(red: 150/256, green: 150/256, blue: 150/256, alpha: 0.8).cgColor
+        cell.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
+        cell.layer.shadowRadius = 3.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.layer.cornerRadius).cgPath
+        cell.layer.backgroundColor = UIColor(red: 245/256, green: 245/256, blue: 245/256, alpha: 1).cgColor
         
         return cell
         
     }
     
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        print("Selected \(indexPath.item)");
-//        let cell = collectionView.cellForItem(at: indexPath)
-//        cell?.layer.borderColor = UIColor.blue.cgColor
-//        cell?.layer.borderWidth = 1
-//        cell?.isSelected = true
-//    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected \(indexPath.item)");
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderColor = UIColor.blue.cgColor
+        cell?.isSelected = true
+    }
 //
 //    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
 //        let cell = collectionView.cellForItem(at: indexPath)
