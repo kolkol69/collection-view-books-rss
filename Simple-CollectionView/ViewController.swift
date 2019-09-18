55b352bab97128308919935d9fc11819801abdc7
kolkol <kolodiy1103@gmail.com>
2019-09-18 08:30:17 +0200

    Add some bg and border coloraturas to cells

--- Simple-CollectionView/ViewController.swift
+++ Simple-CollectionView/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     let store = DataStore.sharedInstance
     
+    let reusableIdentifier = "collectionViewCell";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,15 +30,34 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusableIdentifier, for: indexPath) as! CollectionViewCell
         
         let book = store.audiobooks[indexPath.row]
         
         cell.displayContent(image: store.images[indexPath.row], title: book.name)
+        cell.contentView.backgroundColor = UIColor(red: 240/256, green: 240/256, blue: 240/256, alpha: 0.4)
+        cell.layer.borderColor = UIColor(red: 240/256, green: 240/256, blue: 240/256, alpha: 1).cgColor
+        cell.layer.borderWidth = 1.5
         
         return cell
         
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("Selected \(indexPath.item)");
+//        let cell = collectionView.cellForItem(at: indexPath)
+//        cell?.layer.borderColor = UIColor.blue.cgColor
+//        cell?.layer.borderWidth = 1
+//        cell?.isSelected = true
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+//        let cell = collectionView.cellForItem(at: indexPath)
+//        cell?.layer.borderColor = UIColor.clear.cgColor
+//        cell?.layer.borderWidth = 1
+//        cell?.isSelected = false
+//    }
+    
 
 
 }
